31f96a7234f3a753e5c092b49dab5484cfce1835
Amit K Rao <amitraok@fastmail.in>
2015-05-05 22:53:34 -0700

    added geopy and forecastio

--- geoweather/geoweather.py
+++ geoweather/geoweather.py
@@ -1,10 +1,22 @@
 import click
+import forecastio
+from geopy.geocoders import Nominatim
 
 @click.command()
 @click.option('--address', help='Address String')
 def process_input(address):
     """ Takes and address string and displays a weather profile """
-    click.echo("You Entered "+address)
+
+    # geolocate address from supplied address string
+    geolocator = Nominatim()
+    target_location = geolocator.geocode(address)
+    print "Target Location : "+target_location
+    print "Co-ordinates ({0}, {1})".format(target_location.latitude, target_location.longitude)
+
+    # Call forecastio to get weather details
+    api_key = ""
+    weather_forecast = forecastio.load_forecast(api_key, target_location.latitude, target_location.longitude)
+
 
 if __name__ == '__main__':
     process_input()
