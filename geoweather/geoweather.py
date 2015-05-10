e37d9bca70473a44489728c62d2cb6bb880f9a8c
Amit K Rao <amitraok@fastmail.in>
2015-05-10 12:23:02 -0700

    Moved API key to file

--- geoweather/geoweather.py
+++ geoweather/geoweather.py
@@ -2,6 +2,12 @@ import click
 import forecastio
 from geopy.geocoders import Nominatim
 
+
+def fetch_api_key():
+    with open("apikey.txt") as datafile:
+        return datafile.readlines()[0]
+
+
 @click.command()
 @click.option('--address', help='Address String')
 def process_input(address):
@@ -9,13 +15,24 @@ def process_input(address):
 
     # geolocate address from supplied address string
     geolocator = Nominatim()
-    target_location = geolocator.geocode(address)
-    print "Target Location : "+target_location
+    target_location = geolocator.geocode(address, timeout=5)
+    print "Target Location : "+target_location.address
     print "Co-ordinates ({0}, {1})".format(target_location.latitude, target_location.longitude)
 
     # Call forecastio to get weather details
-    api_key = ""
+    api_key = fetch_api_key()
+    print api_key
+
     weather_forecast = forecastio.load_forecast(api_key, target_location.latitude, target_location.longitude)
+
+    hourly_weather = weather_forecast.hourly()
+    daily_weather = weather_forecast.daily()
+
+    for hourly_data in hourly_weather.data:
+        print hourly_data
+
+    for daily_data in daily_weather.data:
+        print daily_data
 
 
 if __name__ == '__main__':
