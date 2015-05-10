60210b148189bdef6a110d7d1f4c7d4124e18393
Amit K Rao <amitraok@fastmail.in>
2015-05-10 13:05:57 -0700

    Added geopy exception handling, prettytable tables

--- geoweather/geoweather.py
+++ geoweather/geoweather.py
@@ -1,6 +1,8 @@
 import click
 import forecastio
 from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut
+from prettytable import PrettyTable
 
 
 def fetch_api_key():
@@ -15,24 +17,35 @@ def process_input(address):
 
     # geolocate address from supplied address string
     geolocator = Nominatim()
-    target_location = geolocator.geocode(address, timeout=5)
-    print "Target Location : "+target_location.address
-    print "Co-ordinates ({0}, {1})".format(target_location.latitude, target_location.longitude)
+    try:
+        target_location = geolocator.geocode(address, timeout=5)
+        print "Target Location : " + target_location.address
+        print "Co-ordinates ({0}, {1})".format(target_location.latitude, target_location.longitude)
+    except GeocoderTimedOut:
+        print "Error: Geocode failed due to timeout. Please try again in a few minutes"
 
-    # Call forecastio to get weather details
+    # Load API key
     api_key = fetch_api_key()
-    print api_key
 
+    # Load weather for location provided
     weather_forecast = forecastio.load_forecast(api_key, target_location.latitude, target_location.longitude)
 
+    # Process Current weather
     hourly_weather = weather_forecast.hourly()
+    hourly_table = PrettyTable()
+
+    for hourly_data in hourly_weather.data[0:5]:
+        hourly_table.add_column(hourly_data.time.strftime('%H:%M:%S'), [hourly_data.summary])
+    print "Current Weather:"
+    print hourly_table
+
+    # Process weekly forecast
     daily_weather = weather_forecast.daily()
-
-    for hourly_data in hourly_weather.data:
-        print hourly_data
-
+    forecast_table = PrettyTable(["Date", "Forecast Summary"])
     for daily_data in daily_weather.data:
-        print daily_data
+        forecast_table.add_row([daily_data.time.strftime('%m/%d/%Y'), daily_data.summary])
+    print "Forecast for the next few days"
+    print forecast_table
 
 
 if __name__ == '__main__':
